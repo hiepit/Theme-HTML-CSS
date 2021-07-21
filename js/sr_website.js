38105cdfde2dab08cbe855b4294bf4635164c51c
hiepnh <hoanghiepuit97@gmail.com>
2021-07-21 12:19:34 +0700

    [ADD] pc

--- js/sr_website.js
+++ js/sr_website.js
@@ -1,12 +1,53 @@
 $(document).ready(function() {
 
-
     // Shop Filter
     $("#show_popup_login").click(function() {
         $('.login-popup').toggleClass("active");
         $('#icon-up').toggleClass("active");
         $('#icon-down').toggleClass("active");
     });
+
+    // Init google captcha
+    // grecaptcha.ready(() => {
+    //     grecaptcha.render($('#contactusCaptcha'), {
+    //         'sitekey': '6LcMBpYbAAAAABW3dUvxY-1PHK-Vt106EiQwV0FP'
+    //     });
+    // });
+
+    // Handle check captcha and save contact
+    $('#btn-submit').click(function() {
+        console.log('ssssssssss')
+        var $captcha = $('#contactusCaptcha'),
+            response = grecaptcha.getResponse();
+
+        if (response.length === 0) {
+            $('.msg-error').text("reCAPTCHA is mandatory");
+            if (!$captcha.hasClass("error")) {
+                $captcha.addClass("error");
+            }
+        } else {
+            // TODO: save record
+            $('.msg-error').text('');
+            $captcha.removeClass("error");
+            alert('reCAPTCHA marked');
+        }
+    })
+
+    // image maps use on the website
+    $('#home-map-img').maphilight({
+        strokeColor: 'F6A028',
+        strokeWidth: 2,
+        fill: true,
+        fillColor: 'F6A028',
+        fillOpacity: 0.2,
+    });
+
+    // Handle check captcha and save contact
+    // $('#btn-cancel').click(function() {
+    //     console.log('Cancle')
+    //     var contact_form = $('#contact-form')
+    //     inputs = contact_form.getElementsByTagName("input");
+    // })
 
     // if ($(".popup-youtube") && $(".popup-youtube").length > 0) {
     //     $(".popup-youtube").magnificPopup({
---
+++ note.md
@@ -0,0 +1,11 @@
+https://fontawesome.com/v5.15/icons?d=gallery&p=2&q=chevron%20up
+https://codepen.io/alexcarpenter/pen/LveDx
+https://www.image-maps.com/
+https://www.image-map.net/
+
+
+
+file:///C:/Users/15520/Documents/code/Theme%20web/sandbox/themes.iki-bir.com/sandbox/elements.html#toggle-accordion
+file:///C:/Users/15520/Documents/code/Theme%20web/safira%20retail%201/demo.hasthemes.com/safira-preview/safira/services.html
+
+https://projects.davidlynch.org/maphilight/docs/demo_world.html

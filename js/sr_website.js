802c479e99bdbbbc1fc9a5ce76739c11ac657053
hiepnh <hoanghiepuit97@gmail.com>
2021-07-27 13:03:48 +0700

    [REF] add send resume

--- js/sr_website.js
+++ js/sr_website.js
@@ -7,13 +7,6 @@ $(document).ready(function() {
         $('#icon-down').toggleClass("active");
     });
 
-    // Init google captcha
-    // grecaptcha.ready(() => {
-    //     grecaptcha.render($('#contactusCaptcha'), {
-    //         'sitekey': '6LcMBpYbAAAAABW3dUvxY-1PHK-Vt106EiQwV0FP'
-    //     });
-    // });
-
     // Handle check captcha and save contact
     $('#btn-submit').click(function() {
         console.log('ssssssssss')
@@ -42,121 +35,67 @@ $(document).ready(function() {
         fillOpacity: 0.2,
     });
 
-    // Handle check captcha and save contact
-    // $('#btn-cancel').click(function() {
-    //     console.log('Cancle')
-    //     var contact_form = $('#contact-form')
-    //     inputs = contact_form.getElementsByTagName("input");
-    // })
-
-    // if ($(".popup-youtube") && $(".popup-youtube").length > 0) {
-    //     $(".popup-youtube").magnificPopup({
-    //         disableOn: 700,
-    //         type: 'iframe',
-    //         mainClass: 'mfp-fade',
-    //         removalDelay: 160,
-    //         preloader: false,
-    //         fixedContentPos: false
-    //     });
-    // }
-
-    // if ($(".slider-popup-img") && $(".slider-popup-img").length > 0) {
-    //     $(".slider-popup-img").magnificPopup({
-    //         type: "image",
-    //         gallery: {
-    //             enabled: true,
-    //         }
-    //     });
-    // }
-    // $('img.theme-slider-gallary').on('load', function(ev) {
-    //     var $link = $(ev.currentTarget);
-    //     var a = $link.parent().find("a");
-    //     a.attr('href', this.src);
-    // });
-    // if ($(".slider-popup-product") && $(".slider-popup-product").length > 0) {
-    //     $(".slider-popup-product").magnificPopup({
-    //         type: "image",
-    //         gallery: {
-    //             enabled: true,
-    //         }
-    //     });
-    // }
+    // Upload file
+    $('#cv').bind('change', function() {
+        var filename = $("#cv").val();
+        if (/^\s*$/.test(filename)) {
+            $("#noFile").text("No file chosen...");
+        } else {
+            $("#noFile").text(filename.replace("C:\\fakepath\\", ""));
+        }
+    });
 
     // Back to Top
     // var offset = 300,
-    // offset_opacity = 1200,
-    // scroll_top_duration = 700,
-    // $back_to_top = $('.cd-top');
-
+    //     scroll_top_duration = 300,
+    //     back_to_top = $('#scrollUp');
     // $(window).scroll(function() {
-    //     ($(this).scrollTop() > offset) ? $back_to_top.addClass('cd-is-visible'): $back_to_top.removeClass('cd-is-visible cd-fade-out');
-    //     if ($(this).scrollTop() > offset_opacity) {
-    //         $back_to_top.addClass('cd-fade-out');
+    //     ($(this).scrollTop() > offset) ? back_to_top.addClass('cd-is-visible'): back_to_top.removeClass('cd-is-visible cd-fade-out');
+    // });
+
+    // $(".scroll").click(function() {
+    //     $("html,body").animate({ scrollTop: $("#header").offset().top }, "1000");
+    //     return false
+    // })
+    // $(window).scroll(function() {
+    //     if ($(this).scrollTop() > 50) {
+    //         $('.scrolltop:hidden').stop(true, true).fadeIn();
+    //     } else {
+    //         $('.scrolltop').stop(true, true).fadeOut();
     //     }
     // });
 
-    //smooth scroll to top
-    // $back_to_top.on('click', function(event) {
-    //     event.preventDefault();
-    //     $('body,html').animate({
-    //         scrollTop: 0,
-    //     }, scroll_top_duration);
-    // });
-
-
-    //Search
-    // $("header .search_open").on("click", function(event) {
-    //     $(".h-search").toggleClass("hm-search-open");
-    //     event.stopPropagation();
-    // });
-    // $(".h-search").on("click", function(event) {
-    //     event.stopPropagation();
-    // });
-    // $(document).on("click", function(event) {
-    //     $(".h-search").removeClass("hm-search-open");
-    // });
-
-    // full width search
-    $(".fw_search_open").click(function() {
-        $('.full-width-search').addClass('fw-open-search');
-    });
-
-    $(".fw_close_search").click(function() {
-        $('.full-width-search').removeClass('fw-open-search');
-    });
-
-    // Mobile Menu
-    $("header .mobile_toggle_menu").click(function() {
-        $(this).toggleClass("open");
-        $('body').toggleClass("mob-main-nav-open");
-    });
-
-
-    /*Mega Menu*/
-    $(".mm-mega-menu > a").after("<span class='mob_menu'></span>")
-    $(".mm-cat-level-1 .cat-level-title").after("<span class='mob_menu'></span>")
-    $(".mm-cat-level-1-v2 .cat-level-title").after("<span class='mob_menu'></span>")
-
-
-    $(".mm-mega-menu .mob_menu").click(function() {
-        $(this).parent('li').toggleClass("open-mob-menu");
-        $(this).toggleClass("mob-menu-open");
-    });
-
-    $(".navbar-top-collapse .dropdown > .dropdown-toggle").after("<span class='mob_menu' data-toggle='dropdown' aria-expanded='false'></span>")
-        /*Mega Menu End*/
-
-
-    $('#getting-started').countdown('2024/03/27', function(event) {
-        var $this = $(this);
-        $this.html(event.strftime('' +
-            '<ul>' +
-            '<li><span>%D</span><label>days</label></li>' +
-            '<li><span>%H</span><label>hr</label></li>' +
-            '<li><span>%M</span><label>min</label></li>' +
-            '<li><span>%S</span><label>sec</label></li>' +
-            '</ul>'
-        ));
+    $.scrollUp({
+        scrollName: 'scrollUp',
+        topDistance: '300',
+        topSpeed: 300,
+        animation: 'fade',
+        animationInSpeed: 200,
+        animationOutSpeed: 200,
+        scrollText: '<i class="fa fa-angle-double-up"></i>',
     });
 
 });
+
+$(window).on("scroll", function() {
+    var HscrollTop = $(window).scrollTop();
+    // Fixed Header
+    if (HscrollTop >= 196) {
+        $('#header').addClass('fixed-header');
+    } else {
+        $('#header').removeClass('fixed-header');
+    }
+});
+
+
+// (function($) {
+//     "use strict"
+// var HscrollTop = $(window).scrollTop();
+//         // Toogle button scorll to top
+//         if (HscrollTop < 300) {
+//             $("#scrollUp").removeClass("cd-is-visible");
+//         } else {
+//             $("#scrollUp").addClass("cd-is-visible");
+//         }
+//     });
+// })(jQuery);
--- note.md
+++ note.md
@@ -3,9 +3,12 @@ https://codepen.io/alexcarpenter/pen/LveDx
 https://www.image-maps.com/
 https://www.image-map.net/
 
+https://codepen.io/your-work
+
 
 
 file:///C:/Users/15520/Documents/code/Theme%20web/sandbox/themes.iki-bir.com/sandbox/elements.html#toggle-accordion
 file:///C:/Users/15520/Documents/code/Theme%20web/safira%20retail%201/demo.hasthemes.com/safira-preview/safira/services.html
+file:///C:/Users/15520/Documents/code/Theme%20web/safira%20retail%201/demo.hasthemes.com/safira-preview/safira/contact.html
 
 https://projects.davidlynch.org/maphilight/docs/demo_world.html

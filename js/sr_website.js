a781a284f7084c78abf67fd24ba3cf62f0718ae4
hiepnh <hoanghiepuit97@gmail.com>
2021-07-29 09:16:04 +0700

    [REF] add licences

--- js/sr_website.js
+++ js/sr_website.js
@@ -45,13 +45,84 @@ $(document).ready(function() {
         }
     });
 
+    // Tooggle list licences
+    $('#select_activity input[type="radio"]').change(function(e) {
+        $('#select_speciality').addClass("toggle_show");
+        var value_selected = e.currentTarget && e.currentTarget.value || '';
+        $('#value_selected_activity span').text(value_selected);
+    });
+    $('#select_activity').click(function() {
+      $('.option_select_activity').toggleClass("active");
+      $('#select_activity .up').toggleClass("active");
+      $('#select_activity .down').toggleClass("active");
+      $('#value_selected_activity').toggleClass("active");
+    });
+
+    $('#select_speciality input[type="radio"]').change(function(e) {
+      $('#select_method').addClass("toggle_show");
+      $('.option_select_method').addClass("active");
+      var value_selected = e.currentTarget && e.currentTarget.value || '';
+      $('#value_selected_speciality span').text(value_selected);
+    });
+    $('#select_speciality').click(function() {
+      $('.option_select_speciality').toggleClass("active");
+      $('#select_speciality .up').toggleClass("active");
+      $('#select_speciality .down').toggleClass("active");
+      $('#value_selected_speciality').toggleClass("active");
+    });
+
+    $('#select_method input[type="checkbox"]').change(function(e) {
+      $('#select_method_2').addClass("toggle_show");
+      $('.option_select_method_2').addClass("active");
+      var value_selected = e.currentTarget && e.currentTarget.value || '';
+      var select_id = e.currentTarget && e.currentTarget.id || '';
+      if (e.currentTarget.checked == true) {
+        // Choose checkbox
+        var divDiv = document.createElement("div");
+        divDiv.id = select_id;
+        var iDiv = document.createElement("i");
+        iDiv.className = 'fas fa-circle';
+        divDiv.append(iDiv);
+        var spanDiv = document.createElement("span");
+        spanDiv.innerHTML = value_selected;
+        divDiv.append(spanDiv);
+        document.getElementById('value_selected_method').append(divDiv);
+      } else {
+        // UnChoose checkbox
+        $('#value_selected_method #'.concat(select_id))[0].remove();
+      }
+    });
+    $('#select_method').click(function() {
+      $('#select_method .up').toggleClass("active");
+      $('#select_method .down').toggleClass("active");
+      $('.option_select_method').toggleClass("active");
+      $('#value_selected_method').toggleClass("active");
+    });
+
+    $('#select_method_2 input[type="radio"]').change(function(e) {
+      var option_name = e.currentTarget &&  e.currentTarget.name || '';
+      var value_selected = e.currentTarget && e.currentTarget.value || '';
+      if (option_name=="option_select_method_1") {
+        $('#value_selected_method_2_1 span').text(value_selected);
+      } else {
+        $('#value_selected_method_2_2 span').text(value_selected);
+      }
+    });
+    $('#select_method_2').click(function() {
+      $('#select_method_2 .up').toggleClass("active");
+      $('#select_method_2 .down').toggleClass("active");
+      $('.option_select_method_2').toggleClass("active");
+      $('#value_selected_method_2_1').toggleClass("active");
+      $('#value_selected_method_2_2').toggleClass("active");
+    });
+
     // Back to Top
-    // var offset = 300,
-    //     scroll_top_duration = 300,
-    //     back_to_top = $('#scrollUp');
-    // $(window).scroll(function() {
-    //     ($(this).scrollTop() > offset) ? back_to_top.addClass('cd-is-visible'): back_to_top.removeClass('cd-is-visible cd-fade-out');
-    // });
+    var offset = 300,
+        scroll_top_duration = 300,
+        back_to_top = $('#scrollUp');
+    $(window).scroll(function() {
+        ($(this).scrollTop() > offset) ? back_to_top.addClass('cd-is-visible'): back_to_top.removeClass('cd-is-visible cd-fade-out');
+    });
 
     // $(".scroll").click(function() {
     //     $("html,body").animate({ scrollTop: $("#header").offset().top }, "1000");
--- note.md
+++ note.md
@@ -5,10 +5,19 @@ https://www.image-map.net/
 
 https://codepen.io/your-work
 
-
+https://freefrontend.com/css-radio-buttons/
 
 file:///C:/Users/15520/Documents/code/Theme%20web/sandbox/themes.iki-bir.com/sandbox/elements.html#toggle-accordion
 file:///C:/Users/15520/Documents/code/Theme%20web/safira%20retail%201/demo.hasthemes.com/safira-preview/safira/services.html
 file:///C:/Users/15520/Documents/code/Theme%20web/safira%20retail%201/demo.hasthemes.com/safira-preview/safira/contact.html
 
 https://projects.davidlynch.org/maphilight/docs/demo_world.html
+
+-- css name
+https://en.bem.info/methodology/key-concepts/
+
+
+-- check Browser is support
+https://caniuse.com/?search=media%20que
+handle on lt (<) IE 9
+https://github.com/scottjehl/Respond
